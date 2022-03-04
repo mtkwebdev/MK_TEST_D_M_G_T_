f8f73d061d20b8bc00f1441fdd0fda3478edd0f0
mtkwebdev <mtkdev1.0@gmail.com>
2022-03-04 22:45:26 +0000

    improved test descriptions

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,15 +1,15 @@
 const {palindrome, readWords, knownPalindromes, notPalindromes} = require('../../public/app')
 
-test('Does readWords() return a single known palindrome name from the list?', ()=>{
+test('Does readWords() return a single known palindrome word from a list?', ()=>{
     const word = knownPalindromes.includes(readWords(knownPalindromes))
     expect(word).toBe(true)
 })
-test('Does readWords() return a single non-palindrome name from the list?', ()=>{
+test('Does readWords() return a single non-palindrome word from a list?', ()=>{
     const word = notPalindromes.includes(readWords(notPalindromes))
     expect(word).toBe(true)
 })
 
-test('Does the palindrome function return true when given palindrome words?', ()=>{
+test('Does the palindrome function return true when given palindrome word?', ()=>{
     expect(palindrome(readWords(knownPalindromes))).toBe(true)
 })
 
