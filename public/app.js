ea1a52dbffc9b16c9c4e9404d61b8bb607735747
mtkwebdev <mtkdev1.0@gmail.com>
2022-03-04 21:23:17 +0000

    set up basic functions and jest tests

---
+++ public/app.js
@@ -0,0 +1,34 @@
+const functions = {
+    palindrome: ()=>{
+        console.log('stuff')
+        return 'stuff'
+    },
+    again: ()=>{
+        console.log('something else')
+        return 'something else'
+    }
+}
+
+
+
+function app(){
+
+
+    // function palindrome(){
+
+    // }
+
+    // function orderByAge(){}
+    // function orderByName(){}
+    // function ageToArray(){}
+    // functions.palindrome()
+
+    // main.addEventListener('clicks',(e)=>{
+    // })
+
+} 
+app()
+
+module.exports = {
+    palindrome: functions.palindrome, 
+    again: functions.again}
--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,1 +1,10 @@
-console.log('something')
+const {palindrome, again} = require('../../public/app')
+
+test('desc', ()=>{
+    expect(palindrome()).toBe('stuff')
+    //expecting test to pass
+})
+test('AGAIN', ()=>{
+    expect(again()).toBe('stuff')
+    //expecting test to fail
+})
