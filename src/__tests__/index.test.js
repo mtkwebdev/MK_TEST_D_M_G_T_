b6bf7f1902b5d02ff79b6ce9d5cbab0357904855
mtkwebdev <mtkdev1.0@gmail.com>
2022-03-04 22:25:37 +0000

    added additional tests, refractored some functions

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,19 @@
-const {palindrome, again, words} = require('../../public/app')
+const {palindrome, readWords, knownPalindromes, notPalindromes} = require('../../public/app')
 
-test('Does the palindrome function work?', ()=>{
-    expect(palindrome(words())).toBe(true)
+test('Does readWords() return a single known palindrome name from the list?', ()=>{
+    const word = knownPalindromes.includes(readWords(knownPalindromes))
+    expect(word).toBe(true)
 })
+test('Does readWords() return a single non-palindrome name from the list?', ()=>{
+    const word = notPalindromes.includes(readWords(notPalindromes))
+    expect(word).toBe(true)
+})
+
+test('Does the palindrome function return true when given palindrome words?', ()=>{
+    expect(palindrome(readWords(knownPalindromes))).toBe(true)
+})
+
+test('Does the palindrome function return false if not given palindromes?', ()=>{
+    expect(palindrome(readWords(notPalindromes))).toBe(false)
+})
+
