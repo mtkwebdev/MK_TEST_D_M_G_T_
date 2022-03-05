6a701cbceaf7822872d377f1dee0a58bb0fe5118
mtkwebdev <mtkdev1.0@gmail.com>
2022-03-05 13:23:14 +0000

    fixed test grammar and cleaned up app

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,14 +1,13 @@
 const {getType} = require('jest-get-type')
 const {palindrome, readWords, knownPalindromes, notPalindromes, sortPeople, people} = require('../../public/app')
 
-test('Does readWords() return a single known palindrome word from a list?', ()=>{
-    const word = knownPalindromes.includes(readWords(knownPalindromes))
-    expect(word).toBe(true)
+test('Does readWords() return a single word from a list?', ()=>{
+    const palindromeWords = knownPalindromes.includes(readWords(knownPalindromes))
+        const justWords = notPalindromes.includes(readWords(notPalindromes))
+    expect(palindromeWords).toBe(true)
+    expect(justWords).toBe(true)
 })
-test('Does readWords() return a single non-palindrome word from a list?', ()=>{
-    const word = notPalindromes.includes(readWords(notPalindromes))
-    expect(word).toBe(true)
-})
+
 
 test('Does the palindrome function return true when given palindrome word?', ()=>{
     expect(palindrome(readWords(knownPalindromes))).toBe(true)
@@ -18,16 +17,18 @@ test('Does the palindrome function return false if not given palindromes?', ()=>
     expect(palindrome(readWords(notPalindromes))).toBe(false)
 })
 
-test('Does sortPeople with byfirstNameOrder return names in String format and in Acending Alphabetical order?', ()=>{
+test('Does sortPeople with byfirstNameOrder return names in String format, and in ascending Alphabetical order?', ()=>{
     const names = sortPeople('byfirstNameOrder').map((details)=> details.name)
     expect(getType(names[0])).toBe('string')
-    expect(names[0].toLowerCase().charAt(0)).toBe('a')
+    // Ideally we would use a hash map of letters and their index position in the alphabet, and then compare alphabet index[i] to index[i+1]
+    // but for now............ we will just test for 'a' as it's the first letter of the alphabet.
+    expect(names[0].toLowerCase().charAt(0)).toBe('a') 
 })
 
 test('Does sortPeople with byfirstNameOrder return the correct names to ages?', ()=>{
     const testCase = {testName: '', testAge: 0, returnedName: '', returnedAge: 0}
     const mapPeople = people.map(person=>{ testCase.testName= person.name, testCase.testAge = person.age})
-    const mapReturned = sortPeople('byfirstNameOrder').map(person=>{
+    const mapsortPeople = sortPeople('byfirstNameOrder').map(person=>{
         if(person.name === testCase.testName){
             return testCase.returnedName = person.name, testCase.returnedAge = person.age
         }
@@ -38,18 +39,16 @@ test('Does sortPeople with byfirstNameOrder return the correct names to ages?', 
 test('Does sortPeople with ageByDescending return ages as numbers, and also sort ages by descending order?', ()=>{
     const ages = sortPeople('ageByDescending').map((details)=> details.age)
     expect(getType(ages[0])).toBe('number')
-    expect(ages).toBeSorted({ descending: true });
+    expect(ages).toBeSorted({ descending: true })
 })
 
-test('Does sortPeople ageByDescending return the correct names?', ()=>{
+test('Does sortPeople with ageByDescending return the correct names for the ages returned?', ()=>{
     const testCase = {testName: '', testAge: 0, returnedName: '', returnedAge: 0}
     const mapPeople = people.map(person=>{ testCase.testName= person.name, testCase.testAge = person.age})
-    const mapReturned = sortPeople('ageByDescending').map(person=>{
+    const mapsortPeople = sortPeople('ageByDescending').map(person=>{
         if(person.name === testCase.testName){
             return testCase.returnedName = person.name, testCase.returnedAge = person.age
         }
         })
         expect(testCase.testName && testCase.testAge).toStrictEqual(testCase.returnedName && testCase.returnedAge)
 })
-
-
